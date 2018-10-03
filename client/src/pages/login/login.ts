60b95f84e33af14455c4c8e791f4508484343f89
Ronan Souza <ronan.souza@ccc.ufcg.edu.br>
2018-10-03 08:39:09 -0300

    Biding de dados da tela de usuário

--- client/src/pages/login/login.ts
+++ client/src/pages/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { CadastroClientePage } from '../cadastro-cliente/cadastro-cliente';
 import { CadastroFornecedorPage } from '../cadastro-fornecedor/cadastro-fornecedor';
 import { RedefinicaoSenhaPage } from '../redefinicao-senha/redefinicao-senha';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
 
 @Component({
   selector: 'page-login',
@@ -10,7 +11,14 @@ import { RedefinicaoSenhaPage } from '../redefinicao-senha/redefinicao-senha';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController) {
+  creds : CredenciaisDTO = {
+    username : "",
+    password : ""
+  };
+
+  constructor(public navCtrl: NavController, 
+    private alertCtrl: AlertController,
+    public loadingCrtl: LoadingController) {
   	
   }
 
@@ -25,4 +33,35 @@ export class LoginPage {
   redefinicaoSenha(){
     this.navCtrl.push(RedefinicaoSenhaPage);
   }
+
+  login(){
+    
+    if (this.creds.username.length < 4){
+      let alert = this.alertCtrl.create({
+        title: "Usuário Inválido",
+        message: "Por favor, digite um usuário válido",
+        buttons:[{
+          text: 'Ok'
+        }]
+      });
+      alert.present();
+      
+      return;
+    }
+    if (this.creds.password.length < 4){
+      let alert = this.alertCtrl.create({
+        title: "Senha Inválida",
+        message: "Por favor, digite uma senha válida",
+        buttons:[{
+          text: 'Ok'
+        }]
+      });
+      alert.present();
+      
+      return;
+    }
+    console.log(this.creds);
+  }
+
 }
+
