2878e12a81e2bde5327c8c87efe45dad726cb4e1
Ronan Souza <ronan.souza@ccc.ufcg.edu.br>
2018-11-01 10:40:54 -0300

    Adiciona model para atualização de dados do usuário

--- client/src/pages/edit-perfil/edit-perfil.ts
+++ client/src/pages/edit-perfil/edit-perfil.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DadosUsuarioDTO } from '../../models/dados-usuario.dto';
 import { UsuarioService } from '../../services/usuario.service';
 import { StorageService } from '../../services/storage.service';
+import { EspecialidadesService } from '../../services/especialidades.service';
+import { DadosAtualizadosDTO } from '../../models/dados-atualizados.dto';
 
 
 
@@ -13,6 +15,8 @@ import { StorageService } from '../../services/storage.service';
 })
 export class EditPerfilPage {
 
+  especialidades : string[] = [];
+
   dadosUsuario : DadosUsuarioDTO = {
     id: null,
     tipo: "",
@@ -21,26 +25,48 @@ export class EditPerfilPage {
     login : "",
     email : ""
   };
+
+  dadosAtualizados : DadosAtualizadosDTO = {
+    novafotoPerfil : "",
+    novoNomeCompleto : "",
+    novoLogin : "",
+    novoEmail : "",
+    novaEspecialidades : []
+  }
   
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     public usuarioService: UsuarioService,
-    public storageService: StorageService) {
+    public storageService: StorageService,
+    public especialidadesService: EspecialidadesService) {
   }
 
   ionViewDidLoad() {
+    this.getEspecialidades();
     this.usuarioService.getMyUser().subscribe(
       response => {
         this.dadosUsuario = response['data'];
-        console.log(this.dadosUsuario);
+        this.dadosAtualizados.novaEspecialidades = this.dadosUsuario.listaEspecialidades;
+        this.dadosAtualizados.novafotoPerfil = this.dadosUsuario.fotoPerfil;
+        this.dadosAtualizados.novoNomeCompleto = this.dadosUsuario.nomeCompleto;
+        this.dadosAtualizados.novoLogin = this.dadosUsuario.login;
+        this.dadosAtualizados.novoEmail = this.dadosUsuario.email;
       }, error => {
         console.log(error);
       });
   }
 
   salvar() {
-    console.log(this.dadosUsuario);
+    console.log(this.dadosAtualizados);
     this.navCtrl.pop();
+  }
+
+  getEspecialidades(){
+    this.especialidadesService.getEspecialidades().subscribe(response => {
+      for (var key in response.body){
+        this.especialidades.push(response.body[key]['nome']);    
+      }
+    });      
   }
 
 }
