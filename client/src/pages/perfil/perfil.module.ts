348311a45b328b855fe5f856e46f83ca84143d6b
Emanuel Brito <emanuel.brito@ccc.ufcg.edu.br>
2018-11-18 11:00:20 -0300

    Ajuste no layout da tela de perfil do usuário

--- client/src/pages/perfil/perfil.module.ts
+++ client/src/pages/perfil/perfil.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { PerfilPage } from './perfil';
+import { BrMaskerModule } from 'brmasker-ionic-3';
+
 
 @NgModule({
   declarations: [
@@ -8,6 +10,7 @@ import { PerfilPage } from './perfil';
   ],
   imports: [
     IonicPageModule.forChild(PerfilPage),
+    BrMaskerModule
   ],
 })
 export class PerfilPageModule {}
