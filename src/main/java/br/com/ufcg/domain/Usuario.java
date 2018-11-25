3b4aefc34da6b25b493b0becee844003c0a07176
Emanuel Brito <emanuel.brito@ccc.ufcg.edu.br>
2018-11-25 10:42:46 -0300

    Realização manual do merge da branch  com a branch

---
+++ src/main/java/br/com/ufcg/domain/Usuario.java
@@ -0,0 +1,188 @@
+package br.com.ufcg.domain;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import br.com.ufcg.dao.UsuarioDAO;
+import br.com.ufcg.domain.enums.TipoUsuario;
+
+@Entity
+@Table(name = "TAB_USUARIO", uniqueConstraints = @UniqueConstraint(columnNames = "TX_LOGIN", name = "login"))
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+public abstract class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID_USUARIO")
+	private Long id;
+
+	@Column(name = "TX_NOME_COMPLETO")
+	private String nomeCompleto;
+
+	@Column(name = "TX_LOGIN")
+	private String login;
+
+	@Column(name = "CD_FOTO_PERFIL", nullable = false)
+	private String fotoPerfil;
+
+	@Column(name = "TX_EMAIL", nullable = false)
+	private String email;
+
+	@Column(name = "TX_SENHA", nullable = false)
+	private String senha;
+
+	@Column(name = "CD_TIPO", nullable = false, updatable = false)
+	@Enumerated
+	private TipoUsuario tipo;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "usuario", fetch = FetchType.EAGER)
+	@Fetch(FetchMode.JOIN)
+	private Set<Avaliacao> avaliacoes;
+
+	public Usuario(String nomeCompleto, String login, String fotoPerfil, String email, String senha, TipoUsuario tipo) {
+		super();
+		this.nomeCompleto = nomeCompleto;
+		this.login = login;
+		this.fotoPerfil = fotoPerfil;
+		this.email = email;
+		this.senha = senha;
+		this.tipo = tipo;
+		this.avaliacoes = new HashSet<>();
+	}
+
+	public Usuario() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Set<Avaliacao> getAvaliacoes() {
+		return this.avaliacoes;
+	}
+
+	public void addAvaliacao(Avaliacao avaliacao) {
+		this.avaliacoes.add(avaliacao);
+	}
+
+	public void setAvaliacoes(Set<Avaliacao> avaliacoes) {
+		this.avaliacoes = avaliacoes;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getFotoPerfil() {
+		return fotoPerfil;
+	}
+
+	public void setFotoPerfil(String fotoPerfil) {
+		this.fotoPerfil = fotoPerfil;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public TipoUsuario getTipo() {
+		return tipo;
+	}
+	
+	protected double getAvaliacaoMedia() {
+		double soma  = 0.0;
+		
+		if (this.avaliacoes != null) {
+			int qtdAvaliacoes = this.avaliacoes.size();
+			
+			if(qtdAvaliacoes > 0) {
+				for(Avaliacao avl : this.avaliacoes) {
+					soma += avl.getNota();
+				}
+				
+				return (soma/qtdAvaliacoes);
+			}
+		}
+		
+		return Double.valueOf(0);
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	public abstract UsuarioDAO toDAO();
+}
